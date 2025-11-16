e037460d085705d47756468f551f6f7b0629f791
g1fttt <george.goworow2015@gmail.com>
2025-11-17 00:23:21 +0100

    fix(utils): handle nil properly inside hl_group_color function

--- lua/utils.lua
+++ lua/utils.lua
@@ -1,19 +1,23 @@
 local M = {}
 
 function M.report_error(msg)
-  vim.api.nvim_echo({{ unpack(msg), "ErrorMsg" }}, true, { err = true })
+  vim.api.nvim_echo({ { unpack(msg), "ErrorMsg" } }, true, { err = true })
 end
 
 function M.hl_group_color(hl_group_name)
   local to_hex = function(rgb_color)
+    if rgb_color == nil then
+      return nil
+    end
+
     return string.format("#%X", rgb_color)
   end
 
   local hl_group_color = vim.api.nvim_get_hl(0, { name = hl_group_name })
 
   return {
-    fg = to_hex(hl_group_color.fg or 0),
-    bg = to_hex(hl_group_color.bg or 0),
+    fg = to_hex(hl_group_color.fg),
+    bg = to_hex(hl_group_color.bg),
   }
 end
 
