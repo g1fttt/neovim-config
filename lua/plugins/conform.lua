f0149f3c701275f3011053a38b131206b43ce81b
g1fttt <george.goworow2015@gmail.com>
2025-12-19 18:54:02 +0100

    fix(prettier): not being found when is installed through bun

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -22,6 +22,18 @@ return {
     formatters = {
       prettier = {
         append_args = { "--no-semi" },
+        command = function(self, bufnr)
+          local fs = require("conform.fs")
+          local util = require("conform.util")
+
+          -- Return type of util.from_node_modules is fun(self: conform.FormatterConfig, ctx: conform.Context): string
+          ---@diagnostic disable: redundant-parameter
+          return
+          -- I'm using bun and it creates prettier.exe in opposite to npm,
+          -- which creates prettier.cmd
+              util.from_node_modules(fs.is_windows and "prettier.exe" or "prettier")(self, bufnr)
+          ---@diagnostic enable
+        end,
       },
     },
   },
